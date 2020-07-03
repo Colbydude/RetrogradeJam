bdb377c15922c48fb68d235eda1a354566d8e020
Colby Terry <colbydude@gmail.com>
2020-07-03 12:24:10 -0700

    Setup window and graphics width and height.

--- Game1.cs
+++ Game1.cs
@@ -8,8 +8,14 @@ namespace RetrogradeJam
 {
     public class Game1 : Game
     {
+        private const int GRAPHICS_WIDTH = 160;
+        private const int GRAPHICS_HEIGHT = 144;
+        private const int WINDOW_WIDTH = 640;
+        private const int WINDOW_HEIGHT = 576;
+
         private EntityManager _entityManager;
         private GraphicsDeviceManager _graphics;
+        private Matrix _graphicsScale;
         private SpriteBatch _spriteBatch;
 
         public Game1()
@@ -22,6 +28,16 @@ namespace RetrogradeJam
 
         protected override void Initialize()
         {
+            // Set up the window.
+            _graphics.PreferredBackBufferWidth = WINDOW_WIDTH;
+            _graphics.PreferredBackBufferHeight = WINDOW_HEIGHT;
+            _graphics.ApplyChanges();
+
+            float scaleX = _graphics.PreferredBackBufferWidth / GRAPHICS_WIDTH;
+            float scaleY = _graphics.PreferredBackBufferHeight / GRAPHICS_HEIGHT;
+
+            _graphicsScale = Matrix.CreateScale(new Vector3(scaleX, scaleY, 1));
+
             // @TEMP
             Texture2D whiteRectangle = new Texture2D(_graphics.GraphicsDevice, 1, 1);
             whiteRectangle.SetData(new[] { Color.White });
@@ -29,7 +45,7 @@ namespace RetrogradeJam
             Entity testEntity = _entityManager.AddEntity("test");
 
             testEntity.AddComponent<Transform>(new Transform(new Vector2(5, 5), new Vector2(1, 1), new Vector2(8, 8)));
-            testEntity.AddComponent<Sprite>(new Sprite(whiteRectangle, 32, 32));
+            testEntity.AddComponent<Sprite>(new Sprite(whiteRectangle, 16, 16));
 
             base.Initialize();
         }
@@ -56,7 +72,7 @@ namespace RetrogradeJam
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            _spriteBatch.Begin();
+            _spriteBatch.Begin(SpriteSortMode.Immediate, null, null, null, null, null, _graphicsScale);
 
                 _entityManager.Draw(_spriteBatch, gameTime);
 
